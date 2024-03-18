470f0d5c8da2a9973be9ef81631849702cf00333
mac <mac@macui-MacBookAir.local>
2024-03-18 20:26:28 +0900

    dom 이용해서 HTML 객체 저장

--- app/app.js
+++ app/app.js
@@ -4,11 +4,19 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 
+// Express 애플리케이션을 생성합니다.
+const app = express();
+
 //1. import.meta.url : 현재 모듈의 URL을 얻습니다
 //2. fileURLToPath 함수를 사용하여 URL을 파일 경로로 변환합니다.
 //3. path.dirname 함수를 사용하여 파일 경로에서 디렉토리 경로를 추출합니다.
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const app = express();
+
+
+// 정적 파일을 제공할 디렉토리를 설정합니다. 
+//path.join() 함수를 사용하여 __dirname과 src/public 경로를 올바르게 연결
+app.use(express.static(path.join(__dirname, "src", "public")));
+
 
 
 // 라우팅
@@ -19,8 +27,6 @@ import home from "./src/routes/home/index.js";
 // 앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "src", "public")));
-
 app.use("/", home); // use -> 미들웨어 등록해주는 메서드
 
 
