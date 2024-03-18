cf64235e032d30df8530a99a1cb7903929897339
mac <mac@macui-MacBookAir.local>
2024-03-18 19:19:47 +0900

    폴더 구조 최적화

---
+++ app/app.js
@@ -0,0 +1,19 @@
+"use strict"
+
+import express from "express";
+const app = express();
+
+
+// 라우팅
+import home from "./src/routes/home/index.js";
+
+
+
+// 앱 세팅
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+app.use("/", home); // use -> 미들웨어 등록해주는 메서드
+
+
+export default app;
